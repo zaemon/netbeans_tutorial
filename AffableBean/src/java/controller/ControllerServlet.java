63078372caa45eda7093f3383eaffd55b072a8a8
zaemon <egawa.tarozaemon@gmail.com>
2015-04-02 00:30:34 +0900

    doGetメソッド実装

--- AffableBean/src/java/controller/ControllerServlet.java
+++ AffableBean/src/java/controller/ControllerServlet.java
@@ -40,7 +40,36 @@ public class ControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        String userPath = request.getServletPath();
         
+        // if category page is requested
+        if (userPath.equals("/category")) {
+            // TODO: Implement category request
+            
+        // if cart page is requested
+        } else if (userPath.equals("/viewCart")) {
+            // TODO: Implement cart page request
+            userPath = "/cart";
+
+            // if checkout page is requested
+        } else if (userPath.equals("/checkout")) {
+            // TODO: Implement checkout page request
+
+            // if user switches language
+        } else if (userPath.equals("/chooseLanguage")) {
+            // TODO: Implement language request
+        }
+        
+        // use RequestDispatcher to forward request internally
+        String url = "/WEB-INF/view" + userPath + ".jsp";
+        
+        try {
+            request.getRequestDispatcher(url).forward(request, response);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
